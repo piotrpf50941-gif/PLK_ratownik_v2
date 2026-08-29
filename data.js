1bea1938dadb6a36e16e26ccdacff5a7c33acb1d
piotrpf50941-gif <piotrpf50941@gmail.com>
2026-08-30 01:24:01 +0200

    Bump app version for internal platform stage

--- data.js
+++ data.js
@@ -4,7 +4,7 @@
   const sourceUrl = 'https://www.erc.edu/science-research/guidelines/guidelines-2025/guidelines-2025-english/';
 
   window.RATOWNIK_DATA = {
-    version: '2.5.0',
+    version: '2.6.0',
     reviewedAt: '2026-08-29',
     quickProcedureIds: ['rko-dorosly', 'krwotok', 'porazenie-pradem', 'wypadek-kolejowy'],
     emergencyChoiceIds: [
