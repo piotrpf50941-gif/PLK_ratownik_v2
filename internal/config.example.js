c04cf301c641a901c35e148002dac1d242597f89
piotrpf50941-gif <piotrpf50941@gmail.com>
2026-08-30 01:17:03 +0200

    Document publishable-only panel configuration

---
+++ internal/config.example.js
@@ -0,0 +1,8 @@
+'use strict';
+
+window.RATOWNIK_INTERNAL_CONFIG = Object.freeze({
+  supabaseUrl: 'https://TWOJ-PROJEKT.supabase.co',
+  supabasePublishableKey: 'sb_publishable_TUTAJ_KLUCZ_PUBLICZNY',
+  environment: 'test',
+  notificationMode: 'simulation'
+});
